895ede7b56e2d04cdc04dd0399c2be9043250062
user1 <sujalpatel2726@gmail.com>
2026-01-23 12:43:39 +0530

    commit

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
-def hello(request):
-    return render(request, 'hello.html', {'name': 'User'})
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
 
-def calc(request):
-    c = ''
-    if request.method == "POST":
-        n1 = int(request.POST['num1'])
-        n2 = int(request.POST['num2'])
-        c = n1 + n2
 
-    return render(request, 'calc.html', {'result': c})
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
